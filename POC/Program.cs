873e9183767625358d42d18817d69fed8c48ffac
Patrick Ens <patrickhacens@gmail.com>
2024-01-12 20:32:43 -0300

    poc code to test serialization

--- POC/Program.cs
+++ POC/Program.cs
@@ -3,34 +3,58 @@ using Daemon.Converters;
 using Daemon.Data;
 using Daemon.Models;
 using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
 
 JsonSerializerOptions jOptions = new()
 {
-    WriteIndented = true,
-    IgnoreReadOnlyProperties = true,
-    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-    Converters = {
-        new PlayerConverter(),
-        new ModifierConverter(),
-        new AttributeConverter(),
-        new SkillConverter(),
-    }
+	WriteIndented = true,
+	IgnoreReadOnlyProperties = true,
+	PropertyNameCaseInsensitive = true,
+	PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+	TypeInfoResolver = new PlayerTypeResolver(),
+	Converters = {
+		new PlayerConverter(),
+		new AttributeConverter(),
+		new SkillConverter(),
+	}
 };
 
 Player player = new()
 {
 
 };
-player[AttributeType.Agility].BaseValue = 10;
-
-PlayerAttribute d = new PlayerAttribute(AttributeType.Agility)
+player.Items.Add(new Item
 {
-    BaseValue = 10
-};
+	Name = "this is a item",
+	Description = "this is description",
+	Weight =1
+});
 
-var sanityCheck = JsonSerializer.Serialize(d, jOptions);
-var sanityCheck2 = JsonSerializer.Deserialize<PlayerAttribute>(sanityCheck, jOptions);
+player.Items.Add(new Weapon
+{
+	Name = "this is a Weapon",
+	Skill = "Some skill",
+	Damage = "1d6",
+	InitiativePenalty = 2,
+	BonusAttribute = AttributeType.Strength,
+	Description = "this is description",
+	Weight = 2
+});
 
+player.Items.Add(new Armor
+{
+	AgiPenalty = 2,
+	DexPenalty = 2,
+	IP = 4,
+	Weight = 3,
+	Description = "somedescription",
+	Name = "armor"
+});
+
+var items = player.Items.ToArray();
+
+var s1 = JsonSerializer.Serialize(items, jOptions);
+var d1 = JsonSerializer.Deserialize<IEnumerable<Item>>(s1, jOptions);
 
 var serialization = JsonSerializer.Serialize(player, jOptions);
 //var serialization = File.OpenRead(@"C:\Users\patri\Downloads\Manuel (15).json");
