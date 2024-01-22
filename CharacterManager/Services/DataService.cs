9c2f138be28df777a71dd41f72045411f600355e
Patrick Ens <patrickhacens@gmail.com>
2024-01-21 23:16:52 -0300

    small changes on character page and add item dialog

--- CharacterManager/Services/DataService.cs
+++ CharacterManager/Services/DataService.cs
@@ -2,35 +2,51 @@
 using System.Text;
 using CsvHelper.Configuration;
 using CsvHelper;
+using System.Diagnostics;
+using MudBlazor;
 
 namespace CharacterManager.Services;
 
 public class DataService
 {
 	private readonly HttpClient httpClient;
+	private readonly ILogger<DataService> logger;
+	private readonly ISnackbar snackbar;
 	private readonly CsvConfiguration csvConfig;
 
-	public DataService(HttpClient httpClient)
+	public DataService(HttpClient httpClient, ILogger<DataService> logger, ISnackbar snackbar)
 	{
 		this.httpClient=httpClient;
+		this.logger=logger;
+		this.snackbar=snackbar;
 		csvConfig = new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)
 		{
 			IgnoreBlankLines =true,
 			HasHeaderRecord = true,
 			HeaderValidated = null,
-			//MissingFieldFound = null,
+			MissingFieldFound = null,
 			AllowComments = true,
 		};
 	}
 
 	private async Task<T[]> GetData<T>(string urlPart, CancellationToken cancellation)
 	{
-		using var response = await httpClient.GetAsync($"data/{urlPart}.csv", cancellation);
-		using var stream = await response.Content.ReadAsStreamAsync(cancellation);
-		using var sr = new StreamReader(stream, Encoding.UTF8);
-		using CsvReader reader = new(sr, csvConfig);
+		try
+		{
 
-		return reader.GetRecords<T>().ToArray();
+			using var response = await httpClient.GetAsync($"data/{urlPart}.csv", cancellation);
+			using var stream = await response.Content.ReadAsStreamAsync(cancellation);
+			using var sr = new StreamReader(stream, Encoding.UTF8);
+			using CsvReader reader = new(sr, csvConfig);
+
+			return reader.GetRecords<T>().ToArray();
+		}
+		catch (Exception ex)
+		{
+			logger.LogError(ex, "Error while getting data");
+			snackbar.Add("Error while getting data", Severity.Error);
+			throw;
+		}
 	}
 
 	public Task<Skill[]> GetWeaponSkills(CancellationToken cancellation = default)
